ea3ee7b68f0cbd4f5eb5f6659fd53c0b3638fba5
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2020-06-02 01:24:25 -0300

    quebrando a kbeca

--- Soma_media.py
+++ Soma_media.py
@@ -1,48 +1,42 @@
-#programa para pedir notas e somar a média
+#programa para pedir notas e somar a média com GUI
 import PySimpleGUI as sg
 
 class SomaMedia:
     def __init__(self):
-        self.nota_one = 0
-        self.nota_two = 0
-        self.nota_three = 0
+        self.nota_one = 'nota_one'
+        self.nota_two = 'nota_two'
+        self.nota_three = 'nota_three'
         self.media = 0
-        layout = [
-            [sg.Text('Nota:'),sg.Input(key=self.nota_one)],
-            [sg.Text('Nota:'),sg.Input(key=self.nota_two)],
-            [sg.Text('Nota:'),sg.Input(key=self.nota_three)]
-        ]
+        #layout
+        self.layout = [
+            [sg.Text('Nota Av1:'),sg.Input(key='nota_one')],
+            [sg.Text('Nota Av2:'),sg.Input(key='nota_two')],
+            [sg.Text('Nota Av3:'),sg.Input(key='nota_three')],
+            [sg.Button('OK')],
+            [sg.Output(size=(20,10))]
+         ]
     def Inicio(self):
         #janela
         self.janela = sg.Window('Soma média',layout = self.layout)
         #ler valores na tela
         self.valores, self.eventos = self.janela.Read()
-        #fazer algo com valores
-        print('Calculadora de Média')
-        print('--'* 10)
-        self.PrimeiraNota()
-        self.SegundaNota()
-        self.TerceiraNota()
-        self.Calculo()
-        if self.media >= 6 and self.media <10:
-            print('You are ok, good')
-        elif self.media == 10:
-            print('Congrulations, you are a good student')
-        elif self.media < 6:
-            print("You are bad, don't cry")
+        #fazer algo com valores   
+        self.Calculo()         
+        try:
+            while True:
+                if self.evento == 'OK':                    
+                    if int(self.media) >= 6 and int(self.media) <10:
+                        print('You are ok, good')
+                    elif int(self.media) == 10:
+                        print('Congrulations, you are a good student')
+                    elif int(self.media) < 6:
+                        print("You are bad, don't cry")
+        except:
+            print('Algo deu errado')
 
-
-    def PrimeiraNota(self):
-        self.nota_one = int(input('Digite a primeira nota: '))
-    
-    def SegundaNota(self):
-        self.nota_two = int(input('Digite a segunda nota: '))
-
-    def TerceiraNota(self):
-        self.nota_three = int(input('Digite a terceira nota: '))
-    
+       
     def Calculo(self):
-        self.media = (self.nota_one + self.nota_three + self.nota_two) / 3
+        self.media = (int(self.nota_one) + int(self.nota_three)+ int(self.nota_two)) / 3
         print(f'A soma das notas {self.nota_one}, {self.nota_two}, {self.nota_three} / 3 = {self.media}')
     
 Media = SomaMedia()
